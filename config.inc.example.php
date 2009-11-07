5cd7d3a825b0a7ee0e28bde632efdd586a26424f
Raphael Michel <webmaster@raphaelmichel.de>
2009-11-07 18:42:18 +0100

    First "real" commit

---
+++ config.inc.example.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ *      config.inc.example.php
+ *      part of PHPWLANstat
+ *
+ *      Author: Raphael Michel <webmaster@raphaelmichel.de>
+ *      Copyright: 2009 geek's factory
+ *      License: MIT-Lizenz
+ */
+define('PWS_INTERFACE', ''); // Das Interface, das genutzt werden
+                             // soll. Zum Herausfinden führe "iwconfig"
+                             // aus und schaue, bei welchem Interface
+                             // etwas wie "IEEE 802.11" dabeisteht ;-)
+
+define('PWS_DATABASE', 'database.sqlite'); // Die zu nutzende Datenbank-
+										   // Datei. Eine leere namens
+										   // "database.sqlite" liegt
+										   // bei.
---
+++ main.php
@@ -0,0 +1,9 @@
+<?php
+/*
+ *      main.php
+ *      part of PHPWLANstat
+ *
+ *      Author: Raphael Michel <webmaster@raphaelmichel.de>
+ *      Copyright: 2009 geek's factory
+ *      License: MIT-Lizenz
+ */
