e914213223d78ed7c0a5fc42167655140b5241c0
Raphael Michel <webmaster@raphaelmichel.de>
2009-11-08 14:14:10 +0100

    HTML statistics

--- statistics.php
+++ statistics.php
@@ -9,7 +9,7 @@
  */
 
 if(!file_exists('config.inc.php'))
-	die('File config.inc.php not found!');
+	die("File config.inc.php not found!\n");
 
 if(!is_dir('stats')) mkdir('stats');
 
@@ -38,24 +38,22 @@ $part_u = $unprotected/$total;
 $arc_p = $part_p*360;
 $arc_u = $part_u*360;
 
-$image = imagecreatetruecolor(380, 200);
+$image = imagecreatetruecolor(380, 202);
 
 // allocate some solors
 $white    = imagecolorallocate($image, 0xFF, 0xFF, 0xFF);
-$red      = imagecolorallocate($image, 0xFF, 0x00, 0x00);
-$darkred  = imagecolorallocate($image, 0x90, 0x00, 0x00);
-$green    = imagecolorallocate($image, 0x00, 0xFF, 0x00);
-$darkgreen= imagecolorallocate($image, 0x00, 0x90, 0x00);
+$darkred  = imagecolorallocate($image, 0x99, 0x00, 0x00);
+$darkgreen= imagecolorallocate($image, 0x00, 0x99, 0x00);
 
+imagefill($image, 0, 0, $white);
 
-// make the 3D effect
 imagefilledarc($image, 100, 100, 200, 200, 0, $arc_p, $darkred, IMG_ARC_PIE);
-imagefilledarc($image, 100, 100, 200, 200, $arc_p, 360 , $green, IMG_ARC_PIE);
+imagefilledarc($image, 100, 100, 200, 200, $arc_p, 360 , $darkgreen, IMG_ARC_PIE);
 
 imagestring($image, 2, 220, 10, 'unprotected accesspoints', $darkgreen);
-imagestring($image, 2, 220, 21, round($part_u*100, 2).'% ('+$unprotected+')', $darkgreen);
+imagestring($image, 2, 220, 21, round(($part_u*100), 2).'% ('.$unprotected.')', $darkgreen);
 imagestring($image, 2, 220, 35, 'protected accesspoints', $darkred);
-imagestring($image, 2, 220, 46, round($part_p*100, 2).'% ('+$protected+')', $darkred);
+imagestring($image, 2, 220, 46, round(($part_p*100), 2).'% ('.$protected.')', $darkred);
 
 
 imagepng($image, 'stats/encrypted.png');
@@ -73,24 +71,22 @@ $part_u = $unk/$total;
 $arc_w = $part_w*360;
 $arc_u = $part_u*360;
 
-$image = imagecreatetruecolor(380, 200);
+$image = imagecreatetruecolor(380, 201);
 
 // allocate some solors
 $white    = imagecolorallocate($image, 0xFF, 0xFF, 0xFF);
-$red      = imagecolorallocate($image, 0xFF, 0x00, 0x00);
-$darkred  = imagecolorallocate($image, 0x90, 0x00, 0x00);
-$grey     = imagecolorallocate($image, 0xFF, 0xFF, 0xFF);
-$darkgrey = imagecolorallocate($image, 0x90, 0x90, 0x90);
+$darkred  = imagecolorallocate($image, 0x99, 0x00, 0x00);
+$darkblue = imagecolorallocate($image, 0x00, 0x00, 0x99);
 
+imagefill($image, 0, 0, $white);
 
-// make the 3D effect
 imagefilledarc($image, 100, 100, 200, 200, 0, $arc_w, $darkred, IMG_ARC_PIE);
-imagefilledarc($image, 100, 100, 200, 200, $arc_w, 360 , $darkgrey, IMG_ARC_PIE);
+imagefilledarc($image, 100, 100, 200, 200, $arc_w, 360 , $darkblue, IMG_ARC_PIE);
 
 imagestring($image, 2, 220, 10, 'WPA', $darkred);
-imagestring($image, 2, 220, 21, ($part_w*100).'% ('+$wpa+')', $darkred);
-imagestring($image, 2, 220, 35, 'unknown', $darkgrey);
-imagestring($image, 2, 220, 46, ($part_u*100).'% ('+$unk+')', $darkgrey);
+imagestring($image, 2, 220, 21, round(($part_w*100), 2).'% ('.$wpa.')', $darkred);
+imagestring($image, 2, 220, 35, 'unknown', $darkblue);
+imagestring($image, 2, 220, 46, round(($part_u*100), 2).'% ('.$unk.')', $darkblue);
 
 
 imagepng($image, 'stats/encryption.png');
@@ -115,15 +111,82 @@ $html = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
 		body, h1, h2, h3 {
 			font-family: sans-serif;
 		}
+		td {
+			border: 1px solid dotted;
+		}
 	</style>
 </head>
 
 <body>';
 
 $html .= '<h1>WL<span style="color:#900;">ANalyse</span> Report</h1>';
+$html .= '<ul>
+	<li><a href="#pie-enc">Percentage of unencrypted WLAN</a></li>
+	<li><a href="#pie-wpa">Percentage of WPA encryption</a></li>
+	<li><a href="#macs-all">50 most detected MAC-Ranges</a></li>
+	<li><a href="#macs-unenc">50 most detected MAC-Ranges on <u>unprotected</u> WLANs</a></li>
+</ul>';
+$html .= '<h2><a name="pie-enc"></a>Percentage of unencrypted WLAN</h2>';
+$html .= '<img src="encrypted.png" alt="Encrypted?" />';
+
+$html .= '<h2><a name="pie-wpa"></a>Percentage of WPA encryption</h2>';
+$html .= '<img src="encryption.png" alt="WPA?" />';
 
 
+if(file_exists('oui.txt')) {
+	$oui_file_content = file_get_contents('oui.txt');
+}
 
+$html .= '<h2><a name="macs-all"></a>50 most detected MAC-Ranges</h2>';
+$html .= '<table>
+			  <tr>
+				  <th>Count</th>
+				  <th>MAC-Range</th>
+				  <th>Owner (OUI)</th>
+			  </tr>';
+
+$query = $db->unbufferedQuery('SELECT macparts as m, COUNT(machash) as c FROM accesspoints GROUP by macparts ORDER by c DESC LIMIT 50');
+$result = $query->fetchAll(SQLITE_ASSOC);
+foreach ($result as $row) {
+	$html .= '<tr><td>'.$row['c'].'</td><td>'.substr($row['m'], 0, 8).'</td>';
+
+	if($oui_file_content){
+		preg_match('#'.str_replace(':', '-', substr($row['m'], 0, 8)).'[^(]*\(hex\)[	 ]*(.*)#i', $oui_file_content, $sub);
+		$html .= '<td>'.$sub[1].'</td>';
+		unset($sub);
+	}else{
+		$html .= '<td></td>';
+	}
+	$html .= '</tr>';
+}
+$html .= '</table>';
+
+$html .= '<h2><a name="macs-unenc"></a>50 most detected MAC-Ranges on <u>unprotected</u> WLANs</h2>';
+$html .= '<table>
+			  <tr>
+				  <th>Count</th>
+				  <th>MAC-Range</th>
+				  <th>Owner (OUI)</th>
+			  </tr>';
+
+$query = $db->unbufferedQuery('SELECT macparts as m, COUNT(machash) as c FROM accesspoints WHERE encrypted = 0 GROUP by macparts ORDER by c DESC LIMIT 50');
+$result = $query->fetchAll(SQLITE_ASSOC);
+foreach ($result as $row) {
+	$html .= '<tr><td>'.$row['c'].'</td><td>'.substr($row['m'], 0, 8).'</td>';
+
+	if($oui_file_content){
+		preg_match('#'.str_replace(':', '-', substr($row['m'], 0, 8)).'[^(]*\(hex\)[	 ]*(.*)#i', $oui_file_content, $sub);
+		$html .= '<td>'.$sub[1].'</td>';
+		unset($sub);
+	}else{
+		$html .= '<td></td>';
+	}
+	$html .= '</tr>';
+}
+$html .= '</table>';
+
+
+$html .= '</body></html>';
 file_put_contents('stats/main.html', $html);
 
 /////////////////////// OK
